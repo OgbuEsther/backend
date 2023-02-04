987d2287b85fa5b3865231d3902b420fd7cf33c4
Ogbu Esther <ogbuuzoma413@gmail.com>
2023-02-04 05:15:55 -0800

    updated

--- utils/appError.ts
+++ utils/appError.ts
@@ -13,8 +13,18 @@ export enum HttpCodes {
     BAD_REQUEST = 400,
     UNAUTHORIZED = 401,
     FORBIDDEN = 403,
+    
     NOT_FOUND = 404,
+    METHOD_NOT_ALLOWED = 405,
+    NOT_ACCEPTABLE = 406,
+    PROXY_AUTHENTICATION_REQUIRED = 407,
+    REQUEST_TIMEOUT = 408,
+    
     CONFLICT = 409,
+    GONE = 410,
+    LENGTH_REQUIRED = 411,
+    
+
     INTERNAL_SERVER_ERROR = 500
 }
 interface AppErrorArgs {
